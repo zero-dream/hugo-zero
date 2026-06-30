6c2d22cc0a52c551d77f03379fbea7cd6099ce64
ZeroDream <140892023+zero-dream@users.noreply.github.com>
2026-06-30 23:45:42 +0800

    refactor(menu): adjust JS for menu expand and collapse

--- assets/js/common/_menu.js
+++ assets/js/common/_menu.js
@@ -7,15 +7,30 @@ const menuEl = document.getElementById("menu");
 if (toggleEl) {
   toggleEl.addEventListener("click", (event) => {
     event.preventDefault();
-    toggleEl.ariaExpanded = menuEl.classList.contains("hidden");
-    menuEl.classList.toggle("hidden");
-
-    if (toggleEl.ariaExpanded === "true") {
-      navEl.classList.remove("blur-01");
-      bodyEl.style.overflow = "hidden";
-    } else {
-      navEl.classList.add("blur-01");
+    toggleEl.ariaExpanded = !menuEl.classList.contains("visible");
+    if (menuEl.classList.contains("visible")) {
       bodyEl.style.overflow = "";
+      navEl.classList.add("blur-01");
+      menuEl.classList.remove("visible");
+      menuEl.style.opacity = "0";
+      menuEl.style.transform = "translateY(-20%)";
+      menuEl.style.visibility = "hidden";
+      menuEl.addEventListener("transitionend", function handler(e) {
+        menuEl.removeEventListener("transitionend", handler);
+        menuEl.style.display = "none";
+      });
+    } else {
+      bodyEl.style.overflow = "hidden";
+      navEl.classList.remove("blur-01");
+      menuEl.classList.add("visible");
+      menuEl.style.display = "flex";
+      menuEl.style.opacity = "0";
+      menuEl.style.transform = "translateY(-20%)";
+      menuEl.style.visibility = "hidden";
+      menuEl.offsetHeight;
+      menuEl.style.opacity = "1";
+      menuEl.style.transform = "translateY(0%)";
+      menuEl.style.visibility = "visible";
     }
   });
 }
